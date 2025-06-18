2d382ab2efd8156c8bd8845c835ddea3f4f641f7
Alexey Stolbov <alstoff@live.ru>
2025-06-18 22:44:29 +0300

    HW_30.1_добавлена админка для наполнения контента

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Course, Lesson
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    """
+    Отображение курсов в админке.
+    """
+    list_display = ('id', 'title', 'description')
+    search_fields = ('title',)
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    """
+    Отображение уроков в админке.
+    """
+    list_display = ('id', 'title', 'course', 'video_url')
+    list_filter = ('course',)
+    search_fields = ('title',)
