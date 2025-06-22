01ed13999f075fb49d8cae0ab0b9c8f439dcc211
Alexey Stolbov <alstoff@live.ru>
2025-06-22 22:29:51 +0300

    HW_30.1_обновлены urls

--- study/urls.py
+++ study/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
 
+app_name = "study"
+
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
-    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
+    path('', include(router.urls)),  # /api/courses/
+    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),  # /api/lessons/
+    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),  # /api/lessons/<id>/
 ]
