fa35e875e9dcae480c4f577ce11e8eca62a63a32
Alexey Stolbov <alstoff@live.ru>
2025-06-23 10:45:35 +0300

    HW_30.1_исправлена структура проекта

--- config/DRFpractice/urls.py
+++ config/DRFpractice/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include(('users.urls', 'users'), namespace='users')),     # <--- вот namespace!
+    #path('api/users/', include(('users.urls', 'users'), namespace='users')),     # <--- вот namespace!
     path('api/', include(('study.urls', 'study'), namespace='study')),           # <--- вот namespace!
 ]
 
