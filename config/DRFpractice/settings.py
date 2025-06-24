d1a76af84458e2c94f6d7093da7d4bb8f5e8c32b
Alexey Stolbov <alstoff@live.ru>
2025-06-24 17:01:22 +0300

    HW_30.2_обновлены зависимости. добавлен Django-Filter

--- config/DRFpractice/settings.py
+++ config/DRFpractice/settings.py
@@ -64,6 +64,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
+
 ROOT_URLCONF = 'DRFpractice.urls'
 
 TEMPLATES = [
