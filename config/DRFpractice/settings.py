90f4ae1787740084a18c34de7825164aadd1e4fd
Alexey Stolbov <alstoff@live.ru>
2025-06-26 15:42:21 +0300

    HW_30.2_Django_fliter добавлен в installed_filter

--- config/DRFpractice/settings.py
+++ config/DRFpractice/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'study',
     'users',
     'rest_framework',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
