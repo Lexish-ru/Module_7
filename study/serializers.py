837de387e6be2e375ede7b59b54c63d09043d264
Alexey Stolbov <alstoff@live.ru>
2025-07-01 21:37:36 +0300

    HW_32.1_Подключён валидатор

--- study/serializers.py
+++ study/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Course, Lesson
+from .validators import youtube_only_validator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     """
     Сериализатор для модели Lesson.
     """
+    video_url = serializers.URLField(validators=[youtube_only_validator])
     class Meta:
         model = Lesson
         fields = ['id', 'course', 'title', 'description', 'preview', 'video_url']
