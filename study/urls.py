3a08f1d6b1bbdb5fa8cac886bbe917634e866439
Alexey Stolbov <alstoff@live.ru>
2025-07-01 21:50:11 +0300

    HW_32.1_Обновлены маршруты

--- study/urls.py
+++ study/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
+from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView, SubscriptionAPIView
 
 app_name = "study"
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', include(router.urls)),  # /api/courses/
     path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),  # /api/lessons/
     path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),  # /api/lessons/<id>/
+    path('subscribe/', SubscriptionAPIView.as_view(), name='subscribe'), # /api/subscribe/
 ]
