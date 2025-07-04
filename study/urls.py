9f9b21516d4d7d4a281026049344a70380215d9c
Alexey Stolbov <alstoff@live.ru>
2025-07-04 12:05:39 +0300

    HW_32.1_Маршрут к lessons реализован через роутер

--- study/urls.py
+++ study/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView, SubscriptionAPIView
+from .views import CourseViewSet, LessonViewSet, SubscriptionAPIView
 
 app_name = "study"
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
+router.register(r'lessons', LessonViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),  # /api/courses/
-    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),  # /api/lessons/
-    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),  # /api/lessons/<id>/
-    path('subscribe/', SubscriptionAPIView.as_view(), name='subscribe'), # /api/subscribe/
+    path('', include(router.urls)),
+    path('subscribe/', SubscriptionAPIView.as_view(), name='subscribe'),
 ]
